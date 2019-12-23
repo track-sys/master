19189ed07bc1e0af6b05c8cd05b7327427f4a4cc
Nalini Murlidhar Wanjale <nwanjale@altimetrik.com>
2019-12-23 10:43:26 +0530

    Initial Commit for java and FE

---
+++ ticketmanagementsystem/src/main/java/com/altimetrik/ticketmanagementsystem/TicketmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.ticketmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ ticketmanagementsystem/src/test/java/com/altimetrik/ticketmanagementsystem/TicketmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.ticketmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
